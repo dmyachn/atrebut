ce8171ccd6509b2a21e9cb6931bf01a0dbe37db7
Dmitry <d.myachn@gmail.com>
2023-10-03 11:43:44 +0300

    done

--- Atrebut/ContentView.swift
+++ Atrebut/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var size = 300.0
+    var maraphone: AttributedString {
+        var result = AttributedString("Mарафон ")
+        result.foregroundColor = .gray
+        return result
+    }
+    
+    
+    var swift: AttributedString {
+        var result = AttributedString("по SwiftUI")
+        return result
+    }
+    
+    var father: AttributedString {
+        var result = AttributedString(" «Отцовский пинок»")
+        result.font = .boldSystemFont(ofSize: 35)
+        
+        result.foregroundColor = .blue
+        return result
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            Text(maraphone + swift + father)
+                .frame(width: size)
+                .border(.green)
+            Slider(
+                value: $size,
+                in: 100...300
+            ).padding()
         }
-        .padding()
     }
 }
 
