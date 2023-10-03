281b5e0ef421c8a5d5ebb71bb82631cb0d61daef
Dmitry <d.myachn@gmail.com>
2023-10-03 10:53:13 +0300

    Initial Commit

---
+++ Atrebut/AtrebutApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtrebutApp.swift
+//  Atrebut
+//
+//  Created by MyachinGarpix on 03.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AtrebutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
